76c342de5b7cf7b36ce688abae901c44cbddcda7
omar mussehl <omarmussehl@omars-MacBook-Pro.local>
2020-06-04 23:06:16 -0400

    Initial commit

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => res.send('API Running'))
+app.get('/', (req, res) => res.send('API Running'));
 
-const PORT = process.env.PORT || 5000; /looks for environment variable called PORT, otherwisedefaults to 5000
+const PORT = process.env.PORT || 5000; //looks for environment variable called PORT, otherwisedefaults to 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
-
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
